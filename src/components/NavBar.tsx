5762d24dcfe35921f746218eba3e139ac363fdc8
Lee Donghyun <d@donghyun.cc>
2025-12-14 22:57:17 +0900

    ナビゲーションバーのシャドウを削除してボーダーのみを使用するように変更し、より洗練された外観にしました。これにより全体的なデザインの一貫性が向上し、パフォーマンスも若干改善されました (Remove shadow from navbar for cleaner design)

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,7 +8,7 @@ export default function NavBar() {
   }
 
   return (
-    <nav className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 shadow-lg">
+    <nav className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 z-50">
       <div className="max-w-md mx-auto px-4">
         <div className="flex justify-around items-center h-16">
           <Link
